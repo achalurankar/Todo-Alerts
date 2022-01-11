430e32c7b89b613a65ff410521b5e6eadd2853a1
Achal Urankar <achal_urankar@persistent.com>
2022-01-12 00:29:08 +0530

    no combinators

--- force-app/main/default/lwc/todoLwc/todoLwc.js
+++ force-app/main/default/lwc/todoLwc/todoLwc.js
@@ -3,12 +3,12 @@ import { LightningElement } from 'lwc';
 export default class TodoLwc extends LightningElement {
 
     renderedCallback() {
-        let ul = this.template.querySelector('ul');
-        let code = ul.innerHTML;
-        let newCode = "";
-        for(let i = 0; i < 5; i++) {
-            newCode += code;
-        }
-        ul.innerHTML = newCode;
+        // let ul = this.template.querySelector('ul');
+        // let code = ul.innerHTML;
+        // let newCode = "";
+        // for(let i = 0; i < 5; i++) {
+        //     newCode += code;
+        // }
+        // ul.innerHTML = newCode;
     }
 }
